c679da5f05d36a8fe648fc5b1388d2fd4a560590
Ashwyn8903 <khandel5@purdue..edu>
2018-11-26 16:47:03 -0500

    Final All Working Code

--- src/StringUtils.java
+++ src/StringUtils.java
@@ -17,13 +17,20 @@ public class StringUtils {
             Ex: res = "SFlannery"
                     --> res = "SFlanner"
          */
-        res.substring(0, 8);
+        if (res.length() > 8) {
+            res.substring(0, 8);
+            int location = res.indexOf('-');
+            res = res.substring(0,location);
+        }
         /* Convert the name to lower-case
             Ex: res = "SFlanner"
                     --> res = "sflanner"
          */
-        res.toLowerCase();
+        res = res.toLowerCase();
         /* return the result of our calculation */
+
+
+
         return res;
     }
 
@@ -36,6 +43,6 @@ public class StringUtils {
      */
     public static String replaceStudentUsername(String text, String username) {
         /* Replace every instance of the username with proper message */
-        return text.replaceAll(username, "[DATA EXPUNDGED]");
+        return text.replaceAll(username, "[DATA EXPUNGED]");
     }
 }
